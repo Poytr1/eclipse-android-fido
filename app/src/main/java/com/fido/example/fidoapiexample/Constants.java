3202cf02e76e234b62c3ca75da2b0784d89540d2
Poytr1 <pengcheng.xu@elastic.co>
2019-01-24 11:51:40 +0800

    Initial commit

---
+++ app/src/main/java/com/fido/example/fidoapiexample/Constants.java
@@ -0,0 +1,25 @@
+/*
+ *  Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.fido.example.fidoapiexample;
+
+public class Constants {
+    public static final String SERVER_CLIENT_ID =
+            "533916084229-qmk7s8pcv7u4uhj2coctv9h75rbp66d3.apps.googleusercontent.com";
+
+    public static final String PREF_SIGNED_IN_STATUS = "accountSignedIn";
+    public static final int DEFAULT_TIMEOUT_SECONDS = 30;
+}
